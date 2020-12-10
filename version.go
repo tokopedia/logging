6f34d08df4c5ab288c71f85539df8c1ce243049a
Taufik Rama <achmad.ramadhan@tokopedia.com>
2020-12-10 11:39:21 +0700

    Added 'SetAppVersion()'

--- version.go
+++ version.go
@@ -5,3 +5,8 @@ var buildhash string
 func appVersion() string {
 	return buildhash
 }
+
+// SetAppVersion see https://github.com/tokopedia/logging/issues/17
+func SetAppVersion(v string) {
+	buildhash = v
+}
